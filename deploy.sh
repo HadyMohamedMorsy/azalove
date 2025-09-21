a2f1c56c246e4727bd8ae20bbecfd0e505d0531e
hady mohamed <hady812012@gmail.com>
2025-09-22 01:57:50 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,34 +2,17 @@ SERVER="root@82.112.240.180"
 TARGET_DIR="/var/www/azalove/front"
 APP_NAME="azalove-frontend"
 
-# Navigate to target directory
-cd $TARGET_DIR
-
-# Move node_modules to parent directory
-mv node_modules ../
-
-# Remove all files and directories
-rm -rf *
-
-# Clone the repository
-git clone https://github.com/HadyMohamedMorsy/azalove.git
-
-# Enter the project directory
-cd azalove
-
-# Move all files and directories to parent directory
-mv * ../
-
-# Go back to parent directory
-cd ../
-
-# Remove the empty azalove directory
-rm -rf azalove
-
-# Go back to parent directory
-cd ..
-
-mv node_modules ./front
+# Execute all deployment commands on the server
+ssh $SERVER "cd $TARGET_DIR && \
+             mv node_modules ../ && \
+             rm -rf * && \
+             git clone https://github.com/HadyMohamedMorsy/azalove.git && \
+             cd azalove && \
+             mv * ../ && \
+             cd ../ && \
+             rm -rf azalove && \
+             cd .. && \
+             mv node_modules ./front"
 
 echo "Restarting PM2 process..."
 ssh $SERVER "cd $TARGET_DIR && \
